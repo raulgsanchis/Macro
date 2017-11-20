5d81fabcc5f8ee6e5d820b975b837f2db9a34001
joernih <jorn.halvorsen@gmail.com>
2017-11-20 20:13:20 +0100

    Med themes

--- R/datatrans.R
+++ R/datatrans.R
@@ -1,3 +1,19 @@
+#' @export
+buildweb <- function(webnr=1){
 
+  website <- c('')[webnr]
 
+  pkg <- devtools::as.package(".")
+  rmarkdown::render_site(paste0('webside/'), encoding = 'UTF-8')
+}
 
+gitcomit <- function(message=Sys.Date()){
+  system("git add --all", TRUE)
+  system("git commit -m 'ABC'", TRUE)
+  system("git push 'ABC'", TRUE)
+}
+
+gitconfig <- function(){
+  system("git config --global user.email 'jorn.halvorsen@gmail.com'")
+  system("git config --global user.name 'joernih'")
+}
