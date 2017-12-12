1fcd0255e1aaa64169b08e3b0a29ae711c89c48d
joernih <jorn.halvorsen@gmail.com>
2017-12-12 20:30:32 +0100

    Nye grafer for USA

--- data_raw/freddata.R
+++ data_raw/freddata.R
@@ -10,24 +10,43 @@ library(dplyr)
 api.key = 'd62b9d8d4ce53e56ea04049dc463ac51'  # substitute ... with your API key
 fred <- FredR(api.key)
 str(fred,1)
-macro.series <- fred$series.search("GDP")
+macro.series1 <- fred$series.search("GDP")
 macro.series2 <- fred$series.search("unemployment")
 macro.series3 <- fred$series.search("money")
 macro.series4 <- fred$series.search("inflation")
 
-#Civilian Unemployment Rate
+# Makrotall USA
 usainf <- fred$series.observations(series_id = c('CPIAUCSL', 'CPILFESL')[1])
-usamone <- fred$series.observations(series_id = c('M1')) %>% dplyr::select(date, value) %>% dplyr::mutate(date = as.Date(date), value = as.numeric(value)) %>% arrange(date)
-usagdp <- fred$series.observations(series_id = c('GDPCA')) %>% dplyr::select(date, value)
+usamone <- fred$series.observations(series_id = c('M1'))
+usagdp <- fred$series.observations(series_id = c('GDPCA'))
 usaunem <- rbind(fred$series.observations(series_id = c('M0892AUSM156SNBR')), fred$series.observations(series_id = c('UNRATE')))
 
+tusainf <- usainf %>% dplyr::mutate(date = as.Date(date), value = as.numeric(value)) %>% arrange(date)
+tusamone <- usamone %>% dplyr::mutate(date = as.Date(date), value = as.numeric(value)) %>% arrange(date)
+tusagdp  <- usagdp %>% dplyr::mutate(date = as.Date(date), value = as.numeric(value)) %>% arrange(date)
+tusaunem <- usaunem %>% dplyr::mutate(date = as.Date(date), value = as.numeric(value)) %>% arrange(date)
 
-devtools::use_data(usagdp, overwrite = TRUE)
-devtools::use_data(usaunem, overwrite = TRUE)
-devtools::use_data(usainf, overwrite = TRUE)
-devtools::use_data(usamone, overwrite = TRUE)
 
-dt <- usamone %>%
+devtools::use_data(tusagdp, overwrite = TRUE)
+devtools::use_data(tusaunem, overwrite = TRUE)
+devtools::use_data(tusainf, overwrite = TRUE)
+devtools::use_data(tusamone, overwrite = TRUE)
+
+
+a <- gpmakro(tusaunem)
+
+
+gpmakro <- function(data=tusaunem, startp="1975-07-01", endp =" 2017-12-12", labt = list(x=NULL, y=NULL)){
+  datainp <- dplyr::filter(data, date > startp & date < endp)
+  ggplot(data = datainp, aes(date, value)) + geom_line() + theme_classic() +
+  geom_smooth(method = 'loess', color = 'red', size = 0.5, se = FALSE) +
+  labs(x = labt$x, y = labt$y)
+}
+
+
+
+
+dt <- tusaunem %>%
   select(
     date,
     value
