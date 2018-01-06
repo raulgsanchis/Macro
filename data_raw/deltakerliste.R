54c2a785b502cddc37508c9b1bcb0c0b74b7dda2
joernih <jorn.halvorsen@gmail.com>
2018-01-06 01:05:13 +0100

    Med passord

--- data_raw/deltakerliste.R
+++ data_raw/deltakerliste.R
@@ -5,7 +5,9 @@ library(dplyr)
 deltakerliste <- read_excel(paste0(devtools::as.package(".")$path,'/deltakere/ØKB1115-Makroøkonomi-Studenter-Vår-2018.xlsx'))
 
 # 1. Rense data
-deltakerlistefil <- deltakerliste
+names(deltakerliste) <- c("gendato", "X1", "Fornavn", "Etternavn", "epost", "mobil", "studieprogram", "status")
+
+deltakerlistefil <- deltakerliste %>% dplyr::mutate(passord = substring(epost, 1, 6)) %>% dplyr::select(-X1)
 
 # 2. Lagre data i Rda-format
 devtools::use_data(deltakerlistefil, overwrite = TRUE)
