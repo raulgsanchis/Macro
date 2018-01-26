08b8bf6eb73a260a993fa89d1b46fd4428dbdd43
joernih <jorn.halvorsen@gmail.com>
2018-01-26 15:39:55 +0100

    Før publisering av oppgaver

--- R/kommandoer.R
+++ R/kommandoer.R
@@ -4,6 +4,9 @@ inst <- function(){
   # rmarkdown::render("test.Rmd", "html_document")
   rmarkdown::render(paste0(devtools::as.package(".")$path,'/inst/webside/presentasjoner/pres_innledning.Rmd'))
   rmarkdown::render(paste0(devtools::as.package(".")$path,'/inst/webside/presentasjoner/pres_del1.Rmd'))
+  rmarkdown::render(paste0(devtools::as.package(".")$path,'/inst/webside/presentasjoner/pres_del1.Rmd'))
+  rmarkdown::render(paste0(devtools::as.package(".")$path,'/inst/webside/seminar/seminar1.Rmd'))
+  rmarkdown::render(paste0(devtools::as.package(".")$path,'/inst/webside/seminar/seminar2.Rmd'))
   rmarkdown::render_site(paste0(devtools::as.package(".")$path,'/inst/webside/'))
   #system('cd ..')
   #system('pwd')
@@ -12,8 +15,6 @@ inst <- function(){
 
 
 # system('/home/joernih/gitclones/teaching/ R CMD INSTALL --no-multiarch --with-keep.source MakroOEKB1115')
-
-
 
 # Bygg nettside
 #
