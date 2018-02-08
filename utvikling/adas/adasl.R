b71df8dbb53e995945241ad6ffcf6133c180c650
joernih <jorn.halvorsen@gmail.com>
2018-02-08 18:12:06 +0100

    ADAS python fil fikset

--- utvikling/adas/adasl.R
+++ utvikling/adas/adasl.R
@@ -5,38 +5,24 @@ library(gridExtra)
 library(grid)
 library(latex2exp)
 ###########################################################################################################
-# IS-LM likevekt
-iv <- 0:7.5
-islmexoparvalv <- c(list(c_1 = 0.6, oC = 25, oG= 75, b = 15, oI = 75, T = 10, M= 200, P=1, h = 80, k =2, Y = 300), list(i=c(iv)))
-eislmexoparvalv <- c(list(c_1 = 0.6, oC = 25, oG= 75, b = 15, oI = 75, T = 10, M= 275, P=0.75, h = 80, k =2, Y = 300), list(i=c(iv)))
+# AD-Kurven
+iv <- 0:5
+islmexoparvalv <- c(list(c_1 = 0.6, oC = 50, oG= 50, b = 10, oI = 10, T = 50, M= 100,
+                         P=1, h = 10, k =1, Y = 130), list(i=c(iv)))
+
+eislmexoparvalv <- c(list(c_1 = 0.6, oC = 50, oG= 50, b = 10, oI = 10, T = 50, M= 100,
+                         P=0.75, h = 10, k =1, Y = 130), list(i=c(iv)))
 
 
 dfislm <- dfgeneric(modell='islm', exoparval = islmexoparvalv)
 edfislm <- dfgeneric(modell='islm', exoparval = eislmexoparvalv)
 
-
-### Likevekt ###
-#!: endogenisere gjetteverdier
 dfkurver <- data.frame(kurve=c("IS", "LM"),
                        fargel = c('red', 'red'),
                        fargek = c('red', 'red'),
                        y = c(dfislm$varnavnminverdi$value[c(3)], dfislm$varnavnmaksverdi$value[c(4)]),
                        x = c(dfislm$varnavnminverdi$Iv[c(3)],dfislm$varnavnmaksverdi$Iv[c(4)]))
 
-labelsislm <- list(title = 'IS-LM modellen',
-                   y = 'produksjon, inntekt (Y)',
-                   x = 'rentenivå (i)',
-                   x0 = c(TeX('$i_{0}}$')),
-                   y0 = c(TeX('$Y_{0}$')),
-                   kurver = dfkurver)
-
-islmlikevekt <- genmakrofigure(dfnumeric=dfislm,
-                               variables = c(dfislm$varnavn)[c(3,4)],
-                               labt = labelsislm,
-                               scalejust = list(x=0, y=75))  + coord_flip()
-
-
-### Komparativ statikk ###
 dfkurverislm <- data.frame(kurve=c("LM'"),
                            fargel = c('red'),
                            fargek = c('red'),
@@ -57,12 +43,15 @@ islmchangemoney <- cgenmakrofigure(dfnumeric=dfislm,
                                    labt = labelsislm,
                                    elabt = elabelsislm,
                                    scalejust = list(x=0, y=50),
-                                   limits= list(x=NULL, y=c(50,450))) + coord_flip()
+                                   limits= list(x=NULL, y=c(NULL,NULL))) + coord_flip()
 
-Yv <- 50:410 # Guess
-adasexoparvalv <- c(list(c_1 = 0.6, oC = 25, oG= 75, b = 100, oI = 75,
-                         T = 75, M= 2000, P=1, h = 2, k =2, Pe=1, mu = 0.1,
-                         l_1=-20,l_2=1, z=1, A=0.5, N=400, alpha = 1, Ac = 2), list(Y=c(Yv)))
+###################################
+
+
+# Yv <- 50:410 # Guess
+# adasexoparvalv <- c(list(c_1 = 0.6, oC = 25, oG= 75, b = 100, oI = 75,
+#                          T = 75, M= 2000, P=1, h = 2, k =2, Pe=1, mu = 0.1,
+#                          l_1=-20,l_2=1, z=1, A=0.5, N=400, alpha = 1, Ac = 2), list(Y=c(Yv)))
 
 dfadas <- dfgeneric(modell='adasl', exoparval = adasexoparvalv)
 
