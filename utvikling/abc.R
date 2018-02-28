3bdc895973977526bb69cf6b72ad8cba3518f8ef
joernih <jorn.halvorsen@gmail.com>
2018-02-28 15:57:30 +0100

    Liten endring

--- utvikling/abc.R
+++ utvikling/abc.R
@@ -12,7 +12,7 @@ y <- eval(parse(text=expression('(k/n)^(alpha)')),list(alpha=0.5,k=1,n=1:10))
 plottable <- data.frame(y, x=1:10)
 
 abc <- ggplot(data=plottable) + geom_line(aes(x=x, y=y), arrow=arrow()) + theme_classic()
-
+abc
 
 
 
